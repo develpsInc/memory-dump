249bf6c929fd06f0d14b5cbc2d9f1f5113f74295
Amon Daniel <amon.daniel.91@gmail.com>
2024-11-21 15:37:14 +0000

    mod: added database

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,20 @@
+import { Client, Account, Databases } from "appwrite";
+
+export const appwriteConfig = {
+  projectID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "",
+  url: process.env.NEXT_PUBLIC_APPWRITE_URL || "",
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || "",
+  userCollectionId: process.env.NEXT_PUBLIC_USER_COLLECTION_ID || "",
+  imageCollectionId: process.env.NEXT_PUBLIC_IMAGE_COLLECTION_ID || "",
+  titleCollectionId: process.env.NEXT_PUBLIC_TITLE_COLLECTION_ID || "",
+  descriptionCollectionId:
+    process.env.NEXT_PUBLIC_DESCRIPTION_COLLECTION_ID || "",
+};
+
+export const client = new Client();
+
+client.setProject(appwriteConfig.projectID);
+client.setEndpoint(appwriteConfig.url);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
